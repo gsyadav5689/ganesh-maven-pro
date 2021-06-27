efd9bff766af54624581ff0349fe329492bd73d8
gsyadav5689 <gsyadav2686@gmail.com>
2021-06-27 18:32:49 +0530

    maven project

---
+++ ganesh-maven-pro/src/main/java/maven_pro/ganesh_maven_pro/App.java
@@ -0,0 +1,13 @@
+package maven_pro.ganesh_maven_pro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
